b6efb343ae14748ce50777f71ea3f6b02be4f226
SohanKafle <soka_bca2077@lict.edu.np>
2024-08-11 13:51:13 +0545

    update

--- BufferDemo.java
+++ BufferDemo.java
@@ -1,28 +1,27 @@
 import java.nio.*;
 public class BufferDemo {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args){
         CharBuffer buffer = CharBuffer.allocate(10);
         String text = "bufferDemo";
-        System.out.println("Input Text: " + text);
-        for (int i = 0; i < text.length(); i++)
+        System.out.println("Input text:" +text);
+        for(int i=0; i<text.length();i++)
         {
-           char c =  text.charAt(i);
-           buffer.put(c);
+            char c = text.charAt(i);
+            buffer.put(c);
         }
-        int bufferpos = buffer.position();
-        System.out.println("Position after data written into buffer: " + bufferpos);
+        int buffpos = buffer.position();
+        System.out.println("Position after Data written into buffer:" + buffpos);
         buffer.flip();
-        System.out.println("Reading buffer contents: ");
-        while (buffer.hasRemaining())
+        System.out.println("Reading buffer content:");
+        while (buffer.hasRemaining()) 
         {
-            System.out.print(buffer.get());
+            System.out.println(buffer.get());  
         }
+
         buffer.position(5);
         buffer.mark();
         buffer.position(6);
         buffer.reset();
-        System.out.println("/n Restored buffer position: " +buffer.position());
-
+        System.out.println("Restored bufferposition: " + buffer.position());
     }
 }
