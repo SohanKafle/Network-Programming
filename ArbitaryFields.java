1d12746ad0eafb1ef70c5b543714a1f22d754613
SohanKafle <soka_bca2077@lict.edu.np>
2024-08-14 13:11:12 +0545

    update

---
+++ ArbitaryFields.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.net.*;
+
+public class ArbitaryFields {
+    public static void main(String[] args) {
+        // Define the URL you want to test
+        String[] urls = {"https://www.lict.edu.np"};
+
+        for (String urlString : urls) {
+            try {
+                // Create a URL object from the string
+                URL u = new URL(urlString);
+                // Open a connection to the URL
+                URLConnection uc = u.openConnection();
+                // Print all the headers
+                for (int j = 1;; j++) {
+                    String header = uc.getHeaderField(j);
+                    if (header == null) break;
+                    System.out.println(uc.getHeaderFieldKey(j) + ": " + header);
+                }
+            } catch (MalformedURLException ex) {
+                System.err.println(urlString + " is not a URL I understand.");
+            } catch (IOException ex) {
+                System.err.println(ex);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ ReadFromServer.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.net.*;
+
+public class ReadFromServer {
+    public static void main(String[] args) {
+        // Define the URL you want to connect to
+        String urlString = "https://sohankafle.com.np/";
+
+        try {
+            // Create a URL object
+            URL url = new URL(urlString);
+            // Open a connection to the URL
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            // Set the request method to GET
+            connection.setRequestMethod("GET");
+
+            // Get the response code
+            int responseCode = connection.getResponseCode();
+            System.out.println("Response Code: " + responseCode);
+
+            // Read the response
+            try (InputStream in = new BufferedInputStream(connection.getInputStream());
+                 Reader reader = new InputStreamReader(in)) {
+                int c;
+                while ((c = reader.read()) != -1) {
+                    System.out.print((char) c);
+                }
+            }
+
+        } catch (MalformedURLException e) {
+            System.err.println("The URL is not valid: " + e.getMessage());
+        } catch (IOException e) {
+            System.err.println("An I/O error occurred: " + e.getMessage());
+        }
+    }
+}
